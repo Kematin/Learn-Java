91e6a8559292b42e37510eed4f2176f1d8720079
kematin <kematin@inbox.ru>
2023-09-02 10:32:15 +0500

    learn java

--- Syntax/src/Conversion.java
+++ Syntax/src/Conversion.java
@@ -1,9 +1,9 @@
 public class Conversion {
     public static void main(String[] args) {
-        // Simple conversion
-        byte Byte = 123;
-        short shortFromByte = Byte;
-        int intFromShort = shortFromByte;
+       // Simple conversion
+       byte Byte = 123;
+       short shortFromByte = Byte;
+       int intFromShort = shortFromByte;
 
        // Conversion with lost data
        int intValue = 1024;
@@ -16,5 +16,37 @@ public class Conversion {
 
        // Compiler auto convert values to one type
        double doubleValue = 1d + 1f; // 1f (float) convert to double
+
+       // convert int to char
+       int a = 50;
+       char charA = (char) a;
+       System.out.println(charA);
+
+       referencePrimitive();
+       convertToString();
+       usefullMethods();
+    }
+
+    public static void referencePrimitive() {
+       int primitive = 0;
+       Integer reference = Integer.valueOf(primitive); // convert to reference class (boxing)
+       int backToPrimitive = reference.intValue(); // return reference class to primitive (unboxing)
+       System.out.println(reference);
+    }
+
+    public static void convertToString() {
+       long longFromString = Long.parseLong("12512");
+       String stringFromLong = Long.toString(longFromString);
+       String autoConvertation = "area" + 42;
+       System.out.println(autoConvertation); // area42
+    }
+
+    public static void usefullMethods() {
+        short maxShortValue = Short.MAX_VALUE; // 32767
+        int bitCount = Integer.bitCount(123); // 6
+        boolean isLetter = Character.isLetter('a'); // true
+        float floatInfinity = Float.POSITIVE_INFINITY;
+        double doubleNan = Double.NaN;
+        boolean isNan = Double.isNaN(doubleNan); // true
     }
 }
---
+++ Tasks/src/Unicode.java
@@ -0,0 +1,17 @@
+public class Unicode {
+    public static void main(String[] args) {
+        int num = 32;
+        char unicode = charExpression(num);
+        System.out.println(unicode);
+    }
+    public static char charExpression(int a) {
+        char charValue = '\\';
+        int intValue = charValue;
+        intValue += a;
+        char returnValue = (char) intValue;
+        return returnValue;
+    }
+    public static char charExpressionBest(int a) {
+        return (char) ('\\' + a);
+    }
+}
